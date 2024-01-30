11389205cce24fadf551df5d5ef66d663536207f
jakub walczak <jakubwalczak@op.pl>
2024-01-30 22:45:51 +0100

    Add a button to increase the value of the count signal

--- src/app/page-1/page-1.component.ts
+++ src/app/page-1/page-1.component.ts
@@ -1,16 +1,21 @@
-import {Component, computed, effect, inject} from '@angular/core';
+import {Component, computed, effect, inject, Signal} from '@angular/core';
 import {DataService} from "../data.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-page-1',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './page-1.component.html',
   styleUrl: './page-1.component.scss'
 })
 export class Page1Component {
 
   #dataService = inject(DataService);
+
+  #doubleValue: Signal<number>;
 
   constructor() {
 
@@ -24,15 +29,20 @@ export class Page1Component {
       console.log(`Effect from Page-1 component - count value ${this.#dataService.count()}`);
     });
 
-    const doubleValue = computed(() => {
+    this.#doubleValue = computed(() => {
       console.log('DoubleValue computed from page-1');
       return this.#dataService.count() * 2;
     })
 
 
-    console.log('First DoubleValue console.log from page-1', doubleValue());
+    console.log('First DoubleValue console.log from page-1', this.#doubleValue());
     this.#dataService.count.update(current => current + 1);
-    console.log('Second DoubleValue console.log from page-1', doubleValue());
+    console.log('Second DoubleValue console.log from page-1', this.#doubleValue());
   }
 
+  increase() {
+    this.#dataService.count.update(current => current + 1);
+    console.log('Increase DoubleValue console.log from page-1', this.#doubleValue());
+    console.log('Increase count console.log from page-1', this.#dataService.count());
+  }
 }
