0bcad89c1a40462c8ed203d1b92c30f797623cf3
jakub walczak <jakubwalczak@op.pl>
2024-01-30 21:50:25 +0100

    Add simple project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {Page1Component} from "./page-1/page-1.component";
+import {Page2Component} from "./page-2/page-2.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'page-1', component: Page1Component},
+  {path: 'page-2', component: Page2Component},
+];
