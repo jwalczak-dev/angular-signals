dc6cee97465daeba339d0c8dcc2a85e68a561f3f
jakub walczak <jakubwalczak@op.pl>
2024-02-12 20:37:06 +0100

    Add some global styles and basic structure for todo elements

--- src/app/page-3/page-3.component.ts
+++ src/app/page-3/page-3.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TodoComponent} from "../todo/todo.component";
 
 @Component({
   selector: 'app-page-3',
   standalone: true,
-  imports: [],
+  imports: [
+    TodoComponent
+  ],
   templateUrl: './page-3.component.html',
   styleUrl: './page-3.component.scss'
 })
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
+
+  public todos = [
+    {
+      id: 0,
+      title: 'task1',
+      description: 'Do shopping',
+      status: 'todo'
+    },
+    {
+      id: 1,
+      title: 'task2',
+      description: 'Clean room',
+      status: 'inProgress'
+    },
+    {
+      id: 2,
+      title: 'task3',
+      description: 'Make a diner',
+      status: 'inProgress'
+    },
+    {
+      id: 3,
+      title: 'task4',
+      description: 'Read a book',
+      status: 'inProgress'
+    },
+    {
+      id: 4,
+      title: 'task5',
+      description: 'Write a letter',
+      status: 'Done'
+    },
+
+  ]
 
 }
