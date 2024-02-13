517e1ede8c32c2ee4ff5c9d25665d31e333426b8
jakub walczak <jakubwalczak@op.pl>
2024-02-13 21:28:01 +0100

    Move data from todo.component to page-3.component

--- src/app/page-3/page-3.component.ts
+++ src/app/page-3/page-3.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
 import {TodoComponent} from "../todo/todo.component";
 
 @Component({
@@ -11,5 +11,50 @@ import {TodoComponent} from "../todo/todo.component";
   styleUrl: './page-3.component.scss'
 })
 export class Page3Component {
+
+  name = signal('Jakub');
+  todos = signal<any>([]);
+
+  constructor() {
+    effect(() => {
+      console.log('Name is:', this.name());
+    })
+
+    const todos = [
+      {
+        id: 0,
+        title: 'task1',
+        description: 'Do shopping',
+        status: 'todo'
+      },
+      {
+        id: 1,
+        title: 'task2',
+        description: 'Clean room',
+        status: 'inProgress'
+      },
+      {
+        id: 2,
+        title: 'task3',
+        description: 'Make a diner',
+        status: 'inProgress'
+      },
+      {
+        id: 3,
+        title: 'task4',
+        description: 'Read a book',
+        status: 'inProgress'
+      },
+      {
+        id: 4,
+        title: 'task5',
+        description: 'Write a letter',
+        status: 'Done'
+      },
+
+    ];
+
+    this.todos.set(todos)
+  }
 
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, effect, input, Input} from '@angular/core';
 import {NgFor} from "@angular/common";
 
 @Component({
@@ -10,38 +10,47 @@ import {NgFor} from "@angular/common";
 })
 export class TodoComponent {
 
-  public todos = [
-    {
-      id: 0,
-      title: 'task1',
-      description: 'Do shopping',
-      status: 'todo'
-    },
-    {
-      id: 1,
-      title: 'task2',
-      description: 'Clean room',
-      status: 'inProgress'
-    },
-    {
-      id: 2,
-      title: 'task3',
-      description: 'Make a diner',
-      status: 'inProgress'
-    },
-    {
-      id: 3,
-      title: 'task4',
-      description: 'Read a book',
-      status: 'inProgress'
-    },
-    {
-      id: 4,
-      title: 'task5',
-      description: 'Write a letter',
-      status: 'Done'
-    },
+  // public todos = [
+  //   {
+  //     id: 0,
+  //     title: 'task1',
+  //     description: 'Do shopping',
+  //     status: 'todo'
+  //   },
+  //   {
+  //     id: 1,
+  //     title: 'task2',
+  //     description: 'Clean room',
+  //     status: 'inProgress'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'task3',
+  //     description: 'Make a diner',
+  //     status: 'inProgress'
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'task4',
+  //     description: 'Read a book',
+  //     status: 'inProgress'
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'task5',
+  //     description: 'Write a letter',
+  //     status: 'Done'
+  //   },
+  //
+  // ]
 
-  ]
+  todos: any = input();
+  name: any = input();
+
+  constructor() {
+    effect(() => {
+      console.log('TODO Name is:', this.name());
+    })
+  }
 
 }
