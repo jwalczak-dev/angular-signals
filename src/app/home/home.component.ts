824f281ed4e7877fb3d4e0cf05a84ca2bb9e2776
jakub walczak <jakubwalczak@op.pl>
2024-02-14 20:32:33 +0100

    Working with ViewChild and ViewChildren

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Directive,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
+import {NgIf} from "@angular/common";
+
+@Directive({
+  standalone: true,
+  selector: 'pane'
+})
+export class Pane {
+  @Input() id!: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: `<p>home works!!!</p>`,
+  imports: [Pane, NgIf],
+  template: `
+    <p>home works!!!</p>
+    <p>Test Paragraph</p>
+    <p #testParagraph>Test Paragraph 2</p>
+
+    <pane id="pane1"></pane>
+    <pane id="pane2"></pane>
+    <pane id="pane3" *ngIf="shouldShow"></pane>
+
+    <button (click)="toggle()">Toggle</button>
+  `,
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  @ViewChild('testParagraph', {static: true}) testParagraph: ElementRef | undefined;
+  @ViewChildren(Pane) panes: QueryList<Pane> | undefined;
+
+  shouldShow = false;
+
+  toggle() {
+    this.shouldShow = !this.shouldShow;
+  }
+
+  ngAfterViewInit() {
+    console.log(this.testParagraph?.nativeElement.innerHTML);
+
+    this.panes?.changes.subscribe((value) => {
+      console.log(value)
+      value.forEach((pane: Pane) => {
+        console.log(pane.id);
+      });
+    })
+  }
 
 }
